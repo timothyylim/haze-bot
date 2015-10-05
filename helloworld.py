3ef56740d26500ce8e7573489039a41625e0ff16
timothylim <timothy_lim@brown.edu>
2015-10-05 20:21:12 +0100

    Clean comments

--- helloworld.py
+++ helloworld.py
@@ -29,37 +29,9 @@ ACCESS_SECRET = 'krRjGLNm9Eydf2jWlsPamW6u3hqxX3sorTX2JNgbZCEuA'#keep the quotes,
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
- 
-# filename=open('helloworld.txt')
-# f=filename.readlines()
-# filename.close()
-
-
 
 is_first_tweet = get_status.is_first(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
-
-# api_reading = scrape_function.test_function()
-
-# set the seed reading
-# print "Enter Scrape function"
-# api_reading_seed = scrape_function.test_function()
-# print "----------------------------------"
-# print "api reading seed is "
-# print api_reading_seed
-
-# if api_reading_seed == "empty result":
-# 	print "checking previous"
-# 	api_reading_seed = scrape_function.previous()
-
-
-# print api_reading_seed
-# print "at " + str(time_now)
-# print "----------------------------------"
-
-# print api_reading_seed
-
-# api.update_status(status = "Hello World!")
 x = 0 
 while x == 0:
 	
@@ -114,29 +86,3 @@ while x == 0:
 		time.sleep(600)
 
 
-
-
-	# elif api_reading != api_reading_seed:
-	# 	print "----------------------------------"
-	# 	# api.update_status(status = api_reading)
-	# 	print "read updated, sleeping for 40 mins"
-	# 	print api_reading
-	# 	print datetime.now()
-	# 	print "----------------------------------"
-	# 	time.sleep(2400)
-	# else:
-	# 	print "----------------------------------"
-	# 	print "reading is the same, going to sleep"
-	# 	print "going to sleep for 15 min:" + str(count)
-	# 	# api.update_status(status = "going to sleep for 15 mins:" + str(count))
-	# 	count += 1 
-	# 	print datetime.now()
-	# 	print "----------------------------------"
-	# 	time.sleep(900)
-
-
-
-
-# for line in f:
-#     api.update_status(status = line)
-#     time.sleep(60)#Tweet every 15 minutes
