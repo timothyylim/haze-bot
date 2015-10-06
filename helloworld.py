039e2ac5f394cc166f6bdcdbbfcca9bbf63bba1e
timothylim <timothy_lim@brown.edu>
2015-10-06 12:07:50 +0100

    Streamline api updating for twitter

--- helloworld.py
+++ helloworld.py
@@ -10,14 +10,14 @@ import tweepy, time, sys
 
 # Scraping packages
 import requests
-from datetime import datetime
-from pytz import timezone    
 from bs4 import BeautifulSoup
 
-# Get time now
 
-malaysia_time = timezone('Asia/Kuala_Lumpur')
-time_now = datetime.now(malaysia_time)
+
+import time
+from pytz import timezone    
+
+from datetime import datetime, timedelta
 
 #argfile = str(sys.argv[1])
  
@@ -30,123 +30,192 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-is_first_tweet = get_status.is_first(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
 
-updated_old = False
-
-x = 0 
-while x == 0:
-	
-	api_reading = scrape_function.test_function()
-
-
-
-	if api_reading != "empty result" and is_new(api_reading) == True:
-		updated_old = False
-		if int(api_reading) >= 50 and int(api_reading) <= 100:
-			print "----------------------------------"
-			print api_reading + "Printing first tweet"
-			message = "API: " + api_reading + " (PJ, Selangor). Pollution levels moderate."
-			api.update_status(status = message)
-			print datetime.now(malaysia_time)
-			print "Sleeping for 20 minutes"
-			print "----------------------------------"
-			time.sleep(1200)
-
-		elif int(api_reading) > 100 and int(api_reading) <= 200:
-			print "----------------------------------"
-			print api_reading + "Printing first tweet"
-			message = "API: " + api_reading + " (PJ, Selangor). Pollution levels unhealthy."
-			api.update_status(status = message)
-			print datetime.now(malaysia_time)
-			print "Sleeping for 20 minutes"
-			print "----------------------------------"
-			time.sleep(1200)
-
-		elif int(api_reading) > 200 and int(api_reading) <= 300:
-			print "----------------------------------"
-			print api_reading + "Printing first tweet"
-			message = "API: " + api_reading + " (PJ, Selangor). Pollution levels very unhealthy."
-			api.update_status(status = message)
-			print datetime.now(malaysia_time)
-			print "Sleeping for 20 minutes"
-			print "----------------------------------"
-			time.sleep(1200)
-
-		elif int(api_reading) > 300:
-			print "----------------------------------"
-			print api_reading + "Printing first tweet"
-			message = "API: " + api_reading + " (PJ, Selangor). Pollution levels hazardous."
-			api.update_status(status = message)
-			print datetime.now(malaysia_time)
-			print "Sleeping for 20 minutes"
-			print "----------------------------------"
-			time.sleep(1200)
-
-	else:
-		if api_reading == "empty result":
-
-			if updated_old == False:
-				updated_old = True
-				api_reading = scrape_function.previous()
-				# api_reading = get_status.return_last(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
-				if int(api_reading) >= 50 and int(api_reading) <= 100:
-					print "----------------------------------"
-					print api_reading + "Printing first tweet"
-					message = "API: " + api_reading + " (PJ, Selangor). Pollution levels moderate."
-					api.update_status(status = message)
-					print datetime.now(malaysia_time)
-					print "Sleeping for 20 minutes"
-					print "----------------------------------"
-					time.sleep(1200)
-
-				elif int(api_reading) > 100 and int(api_reading) <= 200:
-					print "----------------------------------"
-					print api_reading + "Printing first tweet"
-					message = "API: " + api_reading + " (PJ, Selangor). Pollution levels unhealthy."
-					api.update_status(status = message)
-					print datetime.now(malaysia_time)
-					print "Sleeping for 20 minutes"
-					print "----------------------------------"
-					time.sleep(1200)
-
-				elif int(api_reading) > 200 and int(api_reading) <= 300:
-					print "----------------------------------"
-					print api_reading + "Printing first tweet"
-					message = "API: " + api_reading + " (PJ, Selangor). Pollution levels very unhealthy."
-					api.update_status(status = message)
-					print datetime.now(malaysia_time)
-					print "Sleeping for 20 minutes"
-					print "----------------------------------"
-					time.sleep(1200)
-
-				elif int(api_reading) > 300:
-					print "----------------------------------"
-					print api_reading + "Printing first tweet"
-					message = "API: " + api_reading + " (PJ, Selangor). Pollution levels hazardous."
-					api.update_status(status = message)
-					print datetime.now(malaysia_time)
-					print "Sleeping for 20 minutes"
-					print "----------------------------------"
-					time.sleep(1200)
-
-			
-
-		else:
-			print "----------------------------------"
-			print "no results posted yet, sleeping for 10 minutes"
-			print datetime.now(malaysia_time)
-			time.sleep(600)
-
+# is_first_tweet = get_status.is_first(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
 
 
 def is_new(api_reading):
-	print "Printing last: "
-	print get_status.return_last(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 	if api_reading != scrape_function.previous():
 		return True
 	else:
 		return False 
+
+
+
+def get_last_hour(time):
+	last_hour = time - timedelta(hours = 1)
+	return last_hour
+
+def update_twitter(api_reading):
+	if int(api_reading) >= 50 and int(api_reading) <= 100:
+		print "----------------------------------"
+		print api_reading + "Printing first tweet"
+		message = "API: " + api_reading + " (PJ, Selangor). Pollution levels moderate."
+		api.update_status(status = message)
+		print datetime.now(malaysia_time)
+		print "Sleeping for 20 minutes"
+		print "----------------------------------"
+		time.sleep(1200)
+
+	elif int(api_reading) > 100 and int(api_reading) <= 200:
+		print "----------------------------------"
+		print api_reading + "Printing first tweet"
+		message = "API: " + api_reading + " (PJ, Selangor). Pollution levels unhealthy."
+		api.update_status(status = message)
+		print datetime.now(malaysia_time)
+		print "Sleeping for 20 minutes"
+		print "----------------------------------"
+		time.sleep(1200)
+
+	elif int(api_reading) > 200 and int(api_reading) <= 300:
+		print "----------------------------------"
+		print api_reading + "Printing first tweet"
+		message = "API: " + api_reading + " (PJ, Selangor). Pollution levels very unhealthy."
+		api.update_status(status = message)
+		print datetime.now(malaysia_time)
+		print "Sleeping for 20 minutes"
+		print "----------------------------------"
+		time.sleep(1200)
+
+	elif int(api_reading) > 300:
+		print "----------------------------------"
+		print api_reading + "Printing first tweet"
+		message = "API: " + api_reading + " (PJ, Selangor). Pollution levels hazardous."
+		api.update_status(status = message)
+		print datetime.now(malaysia_time)
+		print "Sleeping for 20 minutes"
+		print "----------------------------------"
+		time.sleep(1200)
+
+
+x = 0 
+while x == 0:
+
+	time = datetime.now(timezone('Asia/Kuala_Lumpur'))
+	
+	api_reading = scrape_function.scrape_website(time)
+	
+
+	if api_reading == "empty result":
+		last_hour = get_last_hour(time)
+		api_reading = scrape_function.scrape_website(last_hour)
+		
+		if is_new(api_reading):
+			update_twitter(api_reading)
+
+
+	else:
+		update_twitter(api_reading)
+
+
+	
+
+
+	# api_reading = scrape_function.scrape_website()
+
+
+
+	# if api_reading != "empty result" and is_new(api_reading) == True:
+	# 	updated_old = False
+	# 	if int(api_reading) >= 50 and int(api_reading) <= 100:
+	# 		print "----------------------------------"
+	# 		print api_reading + "Printing first tweet"
+	# 		message = "API: " + api_reading + " (PJ, Selangor). Pollution levels moderate."
+	# 		api.update_status(status = message)
+	# 		print datetime.now(malaysia_time)
+	# 		print "Sleeping for 20 minutes"
+	# 		print "----------------------------------"
+	# 		time.sleep(1200)
+
+	# 	elif int(api_reading) > 100 and int(api_reading) <= 200:
+	# 		print "----------------------------------"
+	# 		print api_reading + "Printing first tweet"
+	# 		message = "API: " + api_reading + " (PJ, Selangor). Pollution levels unhealthy."
+	# 		api.update_status(status = message)
+	# 		print datetime.now(malaysia_time)
+	# 		print "Sleeping for 20 minutes"
+	# 		print "----------------------------------"
+	# 		time.sleep(1200)
+
+	# 	elif int(api_reading) > 200 and int(api_reading) <= 300:
+	# 		print "----------------------------------"
+	# 		print api_reading + "Printing first tweet"
+	# 		message = "API: " + api_reading + " (PJ, Selangor). Pollution levels very unhealthy."
+	# 		api.update_status(status = message)
+	# 		print datetime.now(malaysia_time)
+	# 		print "Sleeping for 20 minutes"
+	# 		print "----------------------------------"
+	# 		time.sleep(1200)
+
+	# 	elif int(api_reading) > 300:
+	# 		print "----------------------------------"
+	# 		print api_reading + "Printing first tweet"
+	# 		message = "API: " + api_reading + " (PJ, Selangor). Pollution levels hazardous."
+	# 		api.update_status(status = message)
+	# 		print datetime.now(malaysia_time)
+	# 		print "Sleeping for 20 minutes"
+	# 		print "----------------------------------"
+	# 		time.sleep(1200)
+
+	# else:
+	# 	if api_reading == "empty result":
+
+	# 		if updated_old == False:
+	# 			updated_old = True
+	# 			api_reading = scrape_function.previous()
+	# 			# api_reading = get_status.return_last(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+	# 			if int(api_reading) >= 50 and int(api_reading) <= 100:
+	# 				print "----------------------------------"
+	# 				print api_reading + "Printing first tweet"
+	# 				message = "API: " + api_reading + " (PJ, Selangor). Pollution levels moderate."
+	# 				api.update_status(status = message)
+	# 				print datetime.now(malaysia_time)
+	# 				print "Sleeping for 20 minutes"
+	# 				print "----------------------------------"
+	# 				time.sleep(1200)
+
+	# 			elif int(api_reading) > 100 and int(api_reading) <= 200:
+	# 				print "----------------------------------"
+	# 				print api_reading + "Printing first tweet"
+	# 				message = "API: " + api_reading + " (PJ, Selangor). Pollution levels unhealthy."
+	# 				api.update_status(status = message)
+	# 				print datetime.now(malaysia_time)
+	# 				print "Sleeping for 20 minutes"
+	# 				print "----------------------------------"
+	# 				time.sleep(1200)
+
+	# 			elif int(api_reading) > 200 and int(api_reading) <= 300:
+	# 				print "----------------------------------"
+	# 				print api_reading + "Printing first tweet"
+	# 				message = "API: " + api_reading + " (PJ, Selangor). Pollution levels very unhealthy."
+	# 				api.update_status(status = message)
+	# 				print datetime.now(malaysia_time)
+	# 				print "Sleeping for 20 minutes"
+	# 				print "----------------------------------"
+	# 				time.sleep(1200)
+
+	# 			elif int(api_reading) > 300:
+	# 				print "----------------------------------"
+	# 				print api_reading + "Printing first tweet"
+	# 				message = "API: " + api_reading + " (PJ, Selangor). Pollution levels hazardous."
+	# 				api.update_status(status = message)
+	# 				print datetime.now(malaysia_time)
+	# 				print "Sleeping for 20 minutes"
+	# 				print "----------------------------------"
+	# 				time.sleep(1200)
+
+			
+
+	# 	else:
+	# 		print "----------------------------------"
+	# 		print "no results posted yet, sleeping for 10 minutes"
+	# 		print datetime.now(malaysia_time)
+	# 		time.sleep(600)
+
+
+
+
+
 
--- scrape_function.py
+++ scrape_function.py
@@ -10,39 +10,29 @@ from pytz import timezone
 import re
 
 
-def get_target_time():
+def get_current_hour(time):
+	current_hour = time.hour 
+	return current_hour
 
 
 
-
-
-
-def test_function():
-
-	print "Scrape function running"
-
-	malaysia_time = timezone('Asia/Kuala_Lumpur')
-	current_hour = str(datetime.now(malaysia_time).hour)
-
-	print "current hour: "
-	print current_hour
-	print "------------------"
+def get_target_time(current_hour):
+	# print "current hour: "
+	# print current_hour
+	# print "------------------"
+	current_hour = str(current_hour)
 	d = datetime.strptime(current_hour, "%H")
 	d = d.strftime("%I:%M%p")
 	target_time = str(d)
+	return target_time
 
-	# print "this is target time:"
-	# print target_time
+
+def generate_url(target_time, current_hour):
 
 	# Navigate to the correct URL
-
 	starting_url = "http://apims.doe.gov.my/v2/"
 	hour_url = ""
 	date_url = ""
-
-	###############
-	# set the hour URL
-	###############
 	current_hour_int = int(current_hour)
 
 
@@ -64,7 +54,7 @@ def test_function():
 	###############
 	# set the date URL
 	###############
-	malaysia_time = datetime.now(malaysia_time)
+	malaysia_time = datetime.now(timezone('Asia/Kuala_Lumpur'))
 	malaysia_date_url = str(malaysia_time.strftime('%Y-%m-%d')) + ".html"
 	date_url = malaysia_date_url
 
@@ -73,12 +63,20 @@ def test_function():
 	###############
 
 	final_url = starting_url+hour_url+date_url
-	# print "------------------"
-	# print "this is the url"
-	# print final_url
-	# print "------------------"
+
+	return final_url
 
 
+
+def scrape_website(time):
+
+	print "Scrape function running"
+
+
+	current_hour = get_current_hour(time)
+	target_time = get_target_time(current_hour)
+
+	final_url = generate_url(target_time, current_hour)
 
 	r = requests.get(final_url)
 	soup = BeautifulSoup(r.content)
@@ -112,16 +110,6 @@ def test_function():
 				value = value.encode('ascii', 'ignore').decode('ascii')
 				string_value = str(value)
 				result[-1].append(string_value)
-
-	
-	# print "----------------"
-	# print "Result table"
-	# print result[0]
-
-	# print "Target time: "
-	# print target_time
-
-	# print "----------------"
 	index = 0
 	
 	for row in result:
@@ -131,28 +119,17 @@ def test_function():
 			break 
 
 
-	# Find PJ's index (44)
-	# for row in result:
-	# 	if 'Nilai' in row:
-	# 		print "Nilai"
-	# 		print result.index(row) 	
-	# 		break 
+	print "Result table: "
+	print result[44][:]
 
-	# print "----------------"
-	# print "index: "
-	# print index 
-	# print "----------------"
-	# print result[12]
+
 	api_reading = result[44][index]
-	# print "----------------"
-	# print "raw API reading: "
-	# print api_reading
-	# print "----------------"
 
 	if 	api_reading == "" or \
 		api_reading == "n/a":
 
 		print "empty result"
+		print "Scrape DONE"
 		return "empty result"
 
 	else:
@@ -361,5 +338,24 @@ def concat_to_digit(string):
 		return concatenated + "PM" 
 
 
-# previous()
+def get_previous_hour(current_hour):
 
+	if current_hour == 0:
+		current_hour = 11
+		return current_hour
+
+	else:
+		current_hour = current_hour -1
+		return current_hour
+
+# previous()
+	# Find PJ's index (44)
+	# for row in result:
+	# 	if 'Nilai' in row:
+	# 		print "Nilai"
+	# 		print result.index(row) 	
+	# 		break 
+
+# target_time = get_target_time()
+# current_hour = get_current_hour()
+# scrape_website(target_time, current_hour)
